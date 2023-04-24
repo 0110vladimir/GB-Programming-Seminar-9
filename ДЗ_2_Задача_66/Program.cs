7b24fb19d87d71b9e17c907d17cc11871e052a2d
VN <0110vladimir@gmail.com>
2023-04-24 21:59:48 +0300

    Добавление ДЗ

---
+++ ДЗ_2_Задача_66/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 66: Выполнить с помощью рекурсии.Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите число M: ");
+int M = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+if(M>N)Console.WriteLine($"Сумма чисел между M и N равна {SumRec(N,M)}");
+if(M<N)Console.WriteLine($"Сумма чисел между M и N равна {SumRec(M,N)}");
+if(M==N) Console.WriteLine("Сумма чисел между M и N равна 0");
+
+
+int SumRec(int a, int b)
+{
+ if (b-1 == a) return 0;
+ else return b-1 + SumRec(a,b - 1);
+}
---
+++ ДЗ_3_Задача_68/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 68: Выполнить с помощью рекурсии.Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+
+Console.Write("Введите число M: ");
+uint M = uint.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число N: ");
+uint N = uint.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Значение функции Аккермана для M и N равно {Akkerman(M,N)}");
+
+uint Akkerman(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
+}
